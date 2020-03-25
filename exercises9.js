33bd88f58d5b221e610d53c1e3b24ffbd5445d02
liandasari17 <62257074+liandasari17@users.noreply.github.com>
2020-03-25 22:15:27 +0700

    adding exercises 9

---
+++ exercises9.js
@@ -0,0 +1,22 @@
+/* Nomor 1
+*/
+function shoutOut() {
+    console.log("Halo Function!");
+  }
+  console.log(shoutOut());
+
+  /* Nomor 2
+  */
+ function calculateMultiply(num1,num2){
+    return num1 * num2;
+  }
+  console.log(calculateMultiply(5,6));
+
+  /* Nomor 3
+  */
+ function processSentence(name='agus', age='30', address='Jln. Malioboro, Yogjakarta', hobby='gaming') {
+
+    console.log(`Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}!`);
+    
+  }
+  console.log(processSentence());
